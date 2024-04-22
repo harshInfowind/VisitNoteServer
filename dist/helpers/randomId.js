37796c5822997750241334bad01193e72f57ed2e
AnitatInfowind <132639737+AnitatInfowind@users.noreply.github.com>
2024-04-22 17:20:48 +0530

    commitBy: Jayesh | dotPhrase backend updates

--- dist/helpers/randomId.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateRandomHexIds = void 0;
-const crypto = require("crypto");
-function generateRandomHexIds() {
-    return crypto.randomBytes(16).toString("hex");
-}
-exports.generateRandomHexIds = generateRandomHexIds;
-//# sourceMappingURL=randomId.js.map
